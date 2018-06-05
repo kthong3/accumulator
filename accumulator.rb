a63119bcd4e50fb1d5c7ce80252118d264a9efd1
kthong3 <kthongsakounh@gmail.com>
2018-06-05 15:37:20 -0500

    iterate through string instead of array

--- accumulator.rb
+++ accumulator.rb
@@ -1,5 +1,16 @@
 def accum(string)
-
+  accum_str = ""
+  i = 0
+  while i < string.length
+    letters = string[i] * (i+1)
+    if i != string.length-1
+      accum_str += letters.capitalize + "-"
+    else
+      accum_str += letters.capitalize
+    end
+    i += 1
+  end
+  accum_str
 end
 
 accum("abcd")    # "A-Bb-Ccc-Dddd"
